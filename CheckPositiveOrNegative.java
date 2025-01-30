b133daa90b3c88cd0525a94961450acb89a8e4db
bessamsirisha <bessamsirisha251@gmail.com>
2025-01-30 18:05:42 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,17 @@
+public class CheckPositiveOrNegative {
+    public static void main(String[]args){
+        int num=912;
+        if (num>0){
+        System.out.println("The number is positive.");
+    }
+  else if(num<0){
+    System.out.println("the number is negative.");
+}
+else
+{
+    System.out.println("the number is zero.");
+}
+    }
+ }
+
+
